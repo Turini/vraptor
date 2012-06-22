638432caae2f2b2ebd7e41b29e7597b40352affc
Otávio Scherer Garcia <otavio@otavio.com.br>
2012-06-22 02:41:36 -0300

    Converting projects to mvn

--- maven_fix.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mvn install:install-file -DgroupId=org.hibernate -DartifactId=hibernate-validator -Dversion=4.0.2.GA -Dclassifier=new -Dpackaging=jar -Dfile=vraptor-core/lib/optional/hibernate/hibernate-validator-4.0.2.GA.jar
-
-
